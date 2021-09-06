db914da9a4618afa5bbafbd9f6e0c713ea8d1ad3
Mark Lagana <info@technisculpt.com>
2021-09-06 18:53:14 +1000

    parting for .sbv file

--- blender-gtts.py
+++ blender-gtts.py
@@ -67,7 +67,7 @@ class ClosedCaptionSet():
 
         if self.filename[-3:len(self.filename)] == 'txt':
             
-            print(".txt file detected")
+            print(".txt file detected") ##### text file parsing #####
             self.file_type = 0
             line_counter = 0
             cc_text = ""
@@ -116,7 +116,7 @@ class ClosedCaptionSet():
             
         elif self.filename[-3:len(self.filename)] == 'srt' and self.text[0][0] is '1' and self.text[1].find('-->') != -1:
             
-            print(".srt file detected")
+            print(".srt file detected") ##### srt file parsing #####
             self.file_type = 1
             line_counter = 0
             cc_text = ""
@@ -129,7 +129,7 @@ class ClosedCaptionSet():
 
                 if len(line) > 0:
 
-                    if line.find('-->') != -1 and line[0].isnumeric: # timecode
+                    if line.find('-->') != -1 and line[0].isnumeric(): # timecode
                         start = line.split("-->")[0]
                         end = line.split("-->")[1]
                         hrs_start = int(start.split(":")[0])
@@ -186,11 +186,82 @@ class ClosedCaptionSet():
                         self.captions.append( Caption(cc_type, cc_name, cc_text, start_time, end_time) )
 
         elif self.filename[-3:len(self.filename)] == 'sbv' and self.text[0].find(',') != -1:
-            print(".sbv file detected")
+            
+            print(".sbv file detected") ##### sbv file parsing #####
+            self.file_type = 1
+            line_counter = 0
+            cc_text = ""
+            cc_type = 1
+            cc_time = 0
+            start_time = 0
+            end_time = 0
+
+            for line in self.text:
+
+                if len(line) > 0:
+
+                    if (line[0].isnumeric() and line.find(':') != -1 # timecode
+                        and line.find(',') != -1):
+
+                        start = line.split(",")[0]
+                        end = line.split(",")[1]
+                        hrs_start = int(start.split(":")[0])
+                        min_start = int(start.split(":")[1])
+                        sec_start = int(start.split(":")[2].split('.')[0])
+                        ms_start = int(start.split(":")[2].split('.')[1])
+
+                        hrs_end = int(end.split(":")[0])
+                        min_end = int(end.split(":")[1])
+                        sec_end = int(end.split(":")[2].split('.')[0])
+                        ms_end = int(end.split(":")[2].split('.')[1])
+
+                        start_time = Time(hrs_start, min_start, sec_start, ms_start)
+                        end_time = Time(hrs_end, min_end, sec_end, ms_end)
+
+                    elif line[0:2].find('>>') != -1: # person
+                        cc_type = 1
+                        cc_name = line[2:len(line)].split(":")[0]
+                        text_tmp = line[2:len(line)].split(":")[1]
+                        cc_text = text_tmp[1:len(text_tmp)]
+                        
+                        newPerson = True
+                        for person in self.people:
+                            if person == cc_name:
+                                newPerson = False
+                                break
+                        
+                        if newPerson:
+                            self.people.append(cc_name)
+
+                    elif line[0] is '[': # event
+                        cc_type = 2
+                        cc_name = ''
+                        cc_text = line.split('[')[1].split(']')[0]
+                    
+                    elif(len(line) > 1): # plain text line
+                        
+                        if len(cc_text) == 0: # no previous line
+                            cc_name = "noname"
+                            cc_type = 0
+                            cc_text = line
+
+                            
+                        else: # second line
+                            cc_text += " " + line
+
+                else: # len(line == 0) equivalent of '\n'
+                    self.captions.append( Caption(cc_type, cc_name, cc_text, start_time, end_time) )
+                    cc_text = ""
+
+                line_counter += 1
+                if line_counter == len(self.text): # on exit
+                    if len(cc_text) > 0:
+                        self.captions.append( Caption(cc_type, cc_name, cc_text, start_time, end_time) )
+
             self.file_type = 2
+
         else:
             print("please try .txt, .srt or .sbv file")
-            self.file_type = -1, ''
 
 
 class CustomPropertyGroup(bpy.types.PropertyGroup):
@@ -203,7 +274,7 @@ class ImportTranscript(Operator, ImportHelper):
     def execute(self, context):
 
         if DEBUG:
-            test_file = r'C:\Users\marco\blender-gtts\tests\transcript_test.srt'
+            test_file = r'C:\Users\marco\blender-gtts\tests\transcript_test.sbv'
             f = Path(bpy.path.abspath(test_file))
             ClosedCaptionSet(f.read_text().split("\n"), test_file)
             return {'FINISHED'}
