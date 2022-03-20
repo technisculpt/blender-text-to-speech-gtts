0479d718615606ec236f71a4d450fc65683896ed
Mark Lagana <technisculpt@gmail.com>
2022-03-20 14:30:11 +1100

    round timestamp export

--- Text to Speech/exports/export_helper.py
+++ Text to Speech/exports/export_helper.py
@@ -1,9 +1,5 @@
 def ensure_two_chars(number):
-    string = str(number)
+    number_rounded = round(number, 4)
+    string = str(number_rounded)
 
-    if len(string) == 1:
-        return '0' + string
-    elif len(string) > 3:
-        return string[0:3]
-    else:
-        return string
+    return string
